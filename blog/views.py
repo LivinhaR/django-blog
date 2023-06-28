1123ef36175cbd12ba31996fa3a2dbc826a5df9d
Livian <livianrodrigues980@gmail.com>
2023-06-28 10:31:23 -0300

    28/06

---
+++ blog/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+from django.http import HttpResponse
+
+def index(request):
+    # return HttpResponse('Olá Django - index')
+    return render(request, 'index.html')
+
+def ola(request):
+    return HttpResponse('Olá Django')
