8c98afaedb55c6cb58248275ba0f76d204b41146
Livian <livianrodrigues980@gmail.com>
2023-08-16 11:10:43 -0300

    admin-16/08

--- webapp/settings.py
+++ webapp/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog.apps.BlogConfig',
+    'blog.apps.BlogConfig',
+    'django_extensions', # Acrescentar
 ]
 
 MIDDLEWARE = [
