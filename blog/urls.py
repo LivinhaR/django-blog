ab1fb1607e0678622dd62cde68341dfa618cf175
Livian <livianrodrigues980@gmail.com>
2023-09-06 11:17:28 -0300

    Respostas JSON para o model Post.

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from blog.views import index, ola
+from blog.views import (index, ola, get_all_posts, get_post)
 
 urlpatterns = [
     path('index/', index, name= "index"),
-    path('ola/', ola, name= "ola")
+    path('ola/', ola, name= "ola"),
+    path('api/posts', get_all_posts, name="posts_data"),
+    path('api/posts/<int:post_id>', get_post, name="post_data"),
 ]
