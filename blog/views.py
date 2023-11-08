fcbf009db3f87353a1f416297f36138b94552d9e
LivinhaR <livianrodrigues980@gmail.com>
2023-11-08 10:44:20 -0300

    Exemplos de views para o model Post

--- blog/views.py
+++ blog/views.py
@@ -1,17 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 
 import json
 from django.core.serializers.json import DjangoJSONEncoder
 from django.contrib import messages
 from django.views.decorators.csrf import csrf_exempt
 from django.views.generic import DetailView, ListView, TemplateView
+from django.views.generic.detail import DetailView
 from blog.models import Post
 from blog.forms import PostModelForm
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
-
 from django.urls import reverse_lazy
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.template.loader import render_to_string
+from django.core.mail import send_mail
+from django.conf import settings
+
 
 # Create your views here.
 
@@ -141,3 +145,45 @@ class PostDeleteView(LoginRequiredMixin, DeleteView):
     def form_valid(self, form):
         messages.success(self.request, self.success_message)
         return super(PostDeleteView, self).form_valid(form)
+
+def post_send(request, post_id):
+    post =  get_object_or_404 (Post, pk-post_id)
+    post_url = reverse_lazy('post_detail', args=[post_id])
+    try:
+        email = request.POST.get('email')
+        if len(email) < 5:
+            raise ValueError('E-mail invalido')
+        
+        link = f'{request._current_scheme_host}{post_url}'
+        template = "post/post_send"
+        text_message = render_to_string(f" {template}.txt", {'post_link': link})
+        html_message = render_to_string(f" {template}.html", {'post_link': link})
+        send_mail(
+            subject="Este assunto pode te interessar!", 
+            message=text_message, 
+            from_email=settings.EMAIL_HOST_USER, 
+            recipient_list=[email], 
+            html_message=html_message,
+        )
+
+        messages.success(
+            request, 'Postagem compartilhada com sucesso.'
+        )
+
+    except ValueError as error:
+        messages.error(request, error)
+    except:
+        messages.error(
+            request, 'Erro ao enviar a mensagem!'
+        )
+
+    return redirect(post_url)
+
+def post_show(request, post_id):
+    post = get_object_or_404(Post, pk=post_id)
+    return render(request, 'post/detail.html', {'post': post})
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'post/detail.html'
+    context_object_name = 'post'
